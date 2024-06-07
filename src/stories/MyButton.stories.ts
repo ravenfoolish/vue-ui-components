2c42badc23325077c95ffeeefa3c0bc912a90762
ravenfoolish <morotojun@gmail.com>
2024-06-07 17:46:15 +0900

    initial commit

---
+++ src/stories/MyButton.stories.ts
@@ -0,0 +1,53 @@
+import MyButton from "../components/MyButton.vue";
+import type { Meta, StoryObj } from "@storybook/vue3";
+
+type Story = StoryObj<typeof MyButton>;
+
+const meta: Meta<typeof MyButton> = {
+  title: "MyButton",
+  component: MyButton,
+};
+
+// 「ボタン」
+export const Default: Story = {
+    render: (args) => ({
+      components: { MyButton },
+      setup() {
+        return { args };
+      },
+      template: "<MyButton v-bind='args' />",
+    }),
+    args: {
+      label: "ボタン",
+    },
+  };
+  
+  // 「ログイン」
+  export const Login: Story = {
+    render: (args) => ({
+      components: { MyButton },
+      setup() {
+        return { args };
+      },
+      template: "<MyButton v-bind='args' />",
+    }),
+    args: {
+      label: "ログイン",
+    },
+  };
+  
+  // 「会員登録」
+  export const SignUp: Story = {
+    render: (args) => ({
+      components: { MyButton },
+      setup() {
+        return { args };
+      },
+      template: "<MyButton v-bind='args' />",
+    }),
+    args: {
+      label: "会員登録",
+    },
+  };
+
+export default meta;
